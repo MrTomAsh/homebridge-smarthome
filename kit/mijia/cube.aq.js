f144a97067648b09d42db2c38aec2c69fd7b77b5
Tomasz Bzymek <t.bzymek@gmail.com>
2018-10-21 19:16:14 +0100

    Changelog: - Philips Xiaomi Bulb (and Candle) fix - Aqara Double Rocker Wireless Switch support - Aqara Cube support

--- kit/mijia/cube.aq.js
+++ kit/mijia/cube.aq.js
@@ -1,78 +1,124 @@
 const Base = require('./base');
 let PlatformAccessory, Accessory, Service, Characteristic, UUIDGen;
-class CubeAq extends Base {
-  constructor(mijia) {
-    super(mijia);
-    PlatformAccessory = mijia.PlatformAccessory;
-    Accessory = mijia.Accessory;
-    Service = mijia.Service;
-    Characteristic = mijia.Characteristic;
-    UUIDGen = mijia.UUIDGen;
-  }
-  /**
- * parse the gateway json msg
- * @param {*json} json 
- * @param {*remoteinfo} rinfo 
- */
-  parseMsg(json, rinfo) {
-    this.mijia.log.info(json);
-    let { cmd, model, sid } = json;
-    let data = JSON.parse(json.data);
-    this.mijia.log.info(data);
-      /*let { voltage, channel_0, channel_1, dual_channel } = data;
-      this.mijia.log.debug(`${model} ${cmd} voltage->${voltage} channel_0->${channel_0} channel_1->${channel_1} dual_channel->${dual_channel}`);
-      this.setSwitch(sid, voltage, channel_0, 0);
-      this.setSwitch(sid, voltage, channel_1, 1);
-      this.setSwitch(sid, voltage, dual_channel, 2);*/
-  }
-  /**
-   * set up Switch(aqara Switch)
-   * @param {*device id} sid 
-   * @param {*device voltage} voltage 
-   * @param {*device status} status 
-   */
-  setSwitch(sid, voltage, status, index) {
-    let uuid = UUIDGen.generate('Aqara Cube ' + sid + '@' + index);
-    let accessory = this.mijia.accessories[uuid];
-    let service;
-    if (!accessory) {
-      //init a new homekit accessory
-      let name = 'Aqara Cube ' + sid.substring(sid.length - 4) + '_' + index;
-      accessory = new PlatformAccessory(name, uuid, Accessory.Categories.PROGRAMMABLE_SWITCH);
-      accessory.getService(Service.AccessoryInformation)
-        .setCharacteristic(Characteristic.Manufacturer, "Aqara")
-        .setCharacteristic(Characteristic.Model, "Aqara Cube")
-        .setCharacteristic(Characteristic.SerialNumber, sid);
-      accessory.on('identify', function (paired, callback) {
-        callback();
-      });
-      service = new Service.StatelessProgrammableSwitch(name);
-      accessory.addService(service, name);
-      accessory.addService(new Service.BatteryService(name), name);
-    } else {
-      service = accessory.getService(Service.StatelessProgrammableSwitch);
-    }
-    accessory.reachable = true;
-    accessory.context.sid = sid;
-    accessory.context.model = 'sensor_cube.aqgl01';
-    this.mijia.log.info(status);
-    if (status != undefined) {
-      var event = service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
-      if (status == 'click' || status == 'both_click') {
-        event.updateValue(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
-      } else if (status == 'double_click') {
-        event.updateValue(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
-      } else if (status == 'long_click') {
-          event.updateValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
-      }
+class SwitchAqWDl extends Base {
 
+    constructor(mijia) {
+        super(mijia);
+        PlatformAccessory = mijia.PlatformAccessory;
+        Accessory = mijia.Accessory;
+        Service = mijia.Service;
+        Characteristic = mijia.Characteristic;
+        UUIDGen = mijia.UUIDGen;
+        this.buttonMap = {};
     }
-    this.setBatteryService(sid, voltage, accessory);
-    if (!this.mijia.accessories[uuid]) {
-      this.mijia.accessories[uuid] = accessory;
-      this.registerAccessory([accessory]);
+    /**
+     * parse the gateway json msg
+     * @param {*json} json
+     * @param {*remoteinfo} rinfo
+     */
+    parseMsg(json, rinfo) {
+        let { cmd, model, sid } = json;
+        let data = JSON.parse(json.data);
+        let { voltage, status, rotate } = data;
+        this.mijia.log.debug(`${model} ${cmd} voltage->${voltage} status->${status} rotate->${rotate}`);
+        this.setSwitch(sid, voltage, status, rotate);
     }
-    return accessory;
-  }
+    /**
+     * set up Switch(aqara Switch)
+     * @param {*device id} sid
+     * @param {*device voltage} voltage
+     * @param {*device status} status
+     */
+    setSwitch(sid, voltage, status, rotate) {
+        let uuid = UUIDGen.generate('Aqara Cube @' + sid);
+        let accessory = this.mijia.accessories[uuid];
+        let name = 'Aqara Cube ' + sid.substring(sid.length - 4);
+
+        if (!accessory) {
+            //init a new homekit accessory
+            accessory = new PlatformAccessory(name, uuid, Accessory.Categories.PROGRAMMABLE_SWITCH);
+            accessory.getService(Service.AccessoryInformation)
+                .setCharacteristic(Characteristic.Manufacturer, "Aqara")
+                .setCharacteristic(Characteristic.Model, "Aqara Cube")
+                .setCharacteristic(Characteristic.SerialNumber, sid);
+            accessory.on('identify', function (paired, callback) {
+                callback();
+            });
+
+            this.createButton(1, accessory, name, "Channel 1", false);
+            this.createButton(2, accessory, name, "Channel 2", false);
+            this.createButton(3, accessory, name, "Rotation", true);
+
+            accessory.addService(new Service.BatteryService(name), name);
+        } else {
+            this.getButton(1, accessory, name, "Channel 1", false);
+            this.getButton(2, accessory, name, "Channel 2", false);
+            this.getButton(3, accessory, name, "Rotation", true);
+        }
+
+        accessory.reachable = true;
+        accessory.context.sid = sid;
+        accessory.context.model = 'sensor_cube.aqgl01';
+
+        // Channels 1 && 2
+        if (status !== undefined) {
+            if (status === 'alert') {
+                this.buttonPressed(1, Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
+            } else if (status === 'flip90') {
+                this.buttonPressed(1, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
+            } else if (status === 'flip180') {
+                this.buttonPressed(1, Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
+            } else if (status === 'shake_air') {
+                this.buttonPressed(2, Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
+            } else if (status === 'move') {
+                this.buttonPressed(2, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
+            } else if (status === 'tap_twice') {
+                this.buttonPressed(2, Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
+            }
+        }
+
+        // Rotate
+        if (rotate !== undefined) {
+            rotate = parseInt(rotate);
+            if (rotate > 0) {
+                this.buttonPressed(3, Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
+            } else if (rotate < 0) {
+                this.buttonPressed(3, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
+            }
+        }
+
+        this.setBatteryService(sid, voltage, accessory);
+        if (!this.mijia.accessories[uuid]) {
+            this.mijia.accessories[uuid] = accessory;
+            this.registerAccessory([accessory]);
+        }
+        return accessory;
+    }
+
+    buttonPressed(buttonIndex, action) {
+        this.buttonMap[buttonIndex]
+            .updateCharacteristic(Characteristic.ProgrammableSwitchEvent, action)
+    }
+
+    getButton(buttonIndex, accessory, name, buttonName, rotation) {
+        this.buttonMap['' + buttonIndex] = accessory.getService(name + " " + buttonName);
+    }
+
+    createButton(buttonIndex, accessory, name, buttonName, rotation) {
+        let service = new Service.StatelessProgrammableSwitch(name + " " + buttonName, buttonName);
+        service.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
+            .setProps({
+                minValue: Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+                maxValue: rotation ? Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS : Characteristic.ProgrammableSwitchEvent.LONG_PRESS,
+                validValues: [
+                    Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+                    Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS
+                ].concat(rotation ? [] : [Characteristic.ProgrammableSwitchEvent.LONG_PRESS])
+            });
+        service.getCharacteristic(Characteristic.ServiceLabelIndex)
+            .setValue(buttonIndex);
+        this.buttonMap['' + buttonIndex] = service;
+        accessory.addService(service, name + " " + buttonName);
+    }
 }
-module.exports = CubeAq;
+module.exports = SwitchAqWDl;
--- kit/mijia/switch.aq.wirelessdoublerocket.js
+++ kit/mijia/switch.aq.wirelessdoublerocket.js
@@ -1,6 +1,7 @@
 const Base = require('./base');
 let PlatformAccessory, Accessory, Service, Characteristic, UUIDGen;
 class SwitchAqWDl extends Base {
+
   constructor(mijia) {
     super(mijia);
     PlatformAccessory = mijia.PlatformAccessory;
@@ -8,6 +9,7 @@ class SwitchAqWDl extends Base {
     Service = mijia.Service;
     Characteristic = mijia.Characteristic;
     UUIDGen = mijia.UUIDGen;
+    this.buttonMap = {};
   }
   /**
  * parse the gateway json msg
@@ -20,94 +22,76 @@ class SwitchAqWDl extends Base {
       let { voltage, channel_0, channel_1, dual_channel } = data;
       this.mijia.log.debug(`${model} ${cmd} voltage->${voltage} channel_0->${channel_0} channel_1->${channel_1} dual_channel->${dual_channel}`);
       this.setSwitch(sid, voltage, channel_0, channel_1, dual_channel);
-      //this.setSwitch(sid, voltage, channel_1, 1);
-      //this.setSwitch(sid, voltage, dual_channel, 2);
   }
   /**
    * set up Switch(aqara Switch)
    * @param {*device id} sid 
    * @param {*device voltage} voltage 
-   * @param {*device status} status 
+   * @param status_0
+   * @param status_1
+   * @param status_dc
    */
   setSwitch(sid, voltage, status_0, status_1, status_dc) {
-    let uuid = UUIDGen.generate('Aqara Double Rocket Wireless Switch @' + sid);
+    let uuid = UUIDGen.generate('Aqara Double Rocker Wireless Switch @' + sid);
     let accessory = this.mijia.accessories[uuid];
-    let service;
+    let name = 'Aqara Double Rocket Wireless Switch ' + sid.substring(sid.length - 4);
+
     if (!accessory) {
-      //init a new homekit accessory
-      let name = 'Aqara Double Rocket Wireless Switch ' + sid.substring(sid.length - 4);
-      accessory = new PlatformAccessory(name, uuid, Accessory.Categories.PROGRAMMABLE_SWITCH);
-      accessory.getService(Service.AccessoryInformation)
-        .setCharacteristic(Characteristic.Manufacturer, "Aqara")
-        .setCharacteristic(Characteristic.Model, "Aqara Wireless Double Rocket Switch")
-        .setCharacteristic(Characteristic.SerialNumber, sid);
-      accessory.on('identify', function (paired, callback) {
-        callback();
-      });
-
-      service = new Service.StatelessProgrammableSwitch(name + " Channel 1", "Channel 1");
-      accessory.addService(service, name + " Channel 1");
-
-        service = new Service.StatelessProgrammableSwitch(name + " Channel 2", "Channel 2");
-        accessory.addService(service, name + " Channel 2");
-
-        service = new Service.StatelessProgrammableSwitch(name + " Both Channels", "Both Channels");
-        accessory.addService(service, name + " Both Channels");
-
-      accessory.addService(new Service.BatteryService(name), name);
-    } else {
-      service = accessory.getService(Service.StatelessProgrammableSwitch);
-    }
-
-    service.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
-        .setProps({
-            minValue: 0,
-            maxValue: 8,
-            validValues: [
-                0,1,2,3,4,5,6,7,8
-            ]
+        //init a new homekit accessory
+        accessory = new PlatformAccessory(name, uuid, Accessory.Categories.PROGRAMMABLE_SWITCH);
+        accessory.getService(Service.AccessoryInformation)
+          .setCharacteristic(Characteristic.Manufacturer, "Aqara")
+          .setCharacteristic(Characteristic.Model, "Aqara Wireless Double Rocker Switch")
+          .setCharacteristic(Characteristic.SerialNumber, sid);
+        accessory.on('identify', function (paired, callback) {
+          callback();
         });
+
+        this.createButton(1, accessory, name, "Channel 1");
+        this.createButton(2, accessory, name, "Channel 2");
+        this.createButton(3, accessory, name, "Both Channels");
+
+        accessory.addService(new Service.BatteryService(name), name);
+    } else {
+        this.getButton(1, accessory, name, "Channel 1");
+        this.getButton(2, accessory, name, "Channel 2");
+        this.getButton(3, accessory, name, "Both Channels");
+    }
 
     accessory.reachable = true;
     accessory.context.sid = sid;
     accessory.context.model = 'remote.b286acn01';
-    this.mijia.log.info("CH1: " + status_0);
-      this.mijia.log.info("CH2: " + status_1);
-      this.mijia.log.info("DCH: " + status_dc);
 
     // Channel 1
     if (status_0 !== undefined) {
-      var event = service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
       if (status_0 === 'click') {
-        event.updateValue(Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
+          this.buttonPressed(1, Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
       } else if (status_0 === 'double_click') {
-        event.updateValue(Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
+          this.buttonPressed(1, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
       } else if (status_0 === 'long_click') {
-          event.updateValue(Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
+          this.buttonPressed(1, Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
       }
     }
 
       // Channel 2
       if (status_1 !== undefined) {
-          var event = service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
           if (status_1 === 'click') {
-              event.updateValue(3); //0
+              this.buttonPressed(2, Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
           } else if (status_1 === 'double_click') {
-              event.updateValue(4); //1
+              this.buttonPressed(2, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
           } else if (status_1 === 'long_click') {
-              event.updateValue(5); //2
+              this.buttonPressed(2, Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
           }
       }
 
       // Dual Channel
       if (status_dc !== undefined) {
-          var event = service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
           if (status_dc === 'both_click') {
-              event.updateValue(6); //0
+              this.buttonPressed(3, Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS); //0
           } else if (status_dc === 'double_both_click') {
-              event.updateValue(7); //1
+              this.buttonPressed(3, Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS); //1
           } else if (status_dc === 'long_both_click') {
-              event.updateValue(8); //2
+              this.buttonPressed(3, Characteristic.ProgrammableSwitchEvent.LONG_PRESS); //2
           }
       }
 
@@ -117,6 +101,33 @@ class SwitchAqWDl extends Base {
       this.registerAccessory([accessory]);
     }
     return accessory;
+  }
+
+  buttonPressed(buttonIndex, action) {
+      this.buttonMap[buttonIndex]
+          .updateCharacteristic(Characteristic.ProgrammableSwitchEvent, action)
+  }
+
+  getButton(buttonIndex, accessory, name, buttonName) {
+      this.buttonMap['' + buttonIndex] = accessory.getService(name + " " + buttonName);
+  }
+
+  createButton(buttonIndex, accessory, name, buttonName) {
+      let service = new Service.StatelessProgrammableSwitch(name + " " + buttonName, buttonName);
+      service.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
+          .setProps({
+              minValue: Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+              maxValue: Characteristic.ProgrammableSwitchEvent.LONG_PRESS,
+              validValues: [
+                  Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+                  Characteristic.ProgrammableSwitchEvent.DOUBLE_PRESS,
+                  Characteristic.ProgrammableSwitchEvent.LONG_PRESS
+              ]
+          });
+      service.getCharacteristic(Characteristic.ServiceLabelIndex)
+          .setValue(buttonIndex);
+      this.buttonMap['' + buttonIndex] = service;
+      accessory.addService(service, name + " " + buttonName);
   }
 }
 module.exports = SwitchAqWDl;
